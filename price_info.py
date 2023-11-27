ac2da881c797798cf6afd6c1da44cc3eeef90f44
Bork <lau_yh.22@ichat.sp.edu.sg>
2023-11-27 22:43:13 +0800

    Recommit

--- price_info.py
+++ price_info.py
@@ -8,9 +8,10 @@ def total_cost_shopping():
     total_cost = 0
     for key in price_list.keys():
         if key in quantity_list:
-            # complete the implementation below:
+            total_cost += price_list[key] * quantity_list[key]
 
     print("total cost = ", total_cost)
+    return total_cost
 
 
 def cost_of_fruits(fruit, quantity):
@@ -18,10 +19,9 @@ def cost_of_fruits(fruit, quantity):
         if key == fruit:
             cost = quantity*price_list[key]
             break
-
     print("cost of ", quantity, fruit, "=", cost)
 
-
+    return cost
 def main():
 
     cost_of_fruits('apple', 10)
--- test_price_info.py
+++ test_price_info.py
@@ -1,8 +1,21 @@
 import unittest
 
+import price_info
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        result=price_info.total_cost_shopping()
+        test=46.75
+        assert test==result
+
+    def test_cost(self):
+        result=0
+        quantity=5
+        type_of_fruit_in_list = 'orange'
+
+        test=price_info.cost_of_fruits(type_of_fruit_in_list,quantity)
+        assert test==7.0
+
 
 if __name__ == '__main__':
     unittest.main()
